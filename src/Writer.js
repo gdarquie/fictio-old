9d9ab59d516a3ed9ebce6988c512caeca61b0c88
gdarquie <gaetan.darquie@gmail.com>
2020-01-22 01:04:19 +0100

    Add button, jquery and alert when clicking keys

--- src/Writer.js
+++ src/Writer.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import $ from "jquery";
 
 export class Writer extends React.Component {
 
+    okAction() {
+        alert('Top!');
+    }
+
+    cancelAction() {
+        alert('Oh non');
+    }
+
     componentDidMount() {
+        $(document.body).on('keydown', this.handleKeyDown);
+
         let input = document.getElementById('writerTextarea');
         input.style.height = window.innerHeight+'px';
         input.style.width = window.innerWidth+'px';
@@ -10,11 +21,25 @@ export class Writer extends React.Component {
         console.log(window.innerHeight);
     }
 
+    saveAction() {
+        alert('Save text');
+    }
+
+    handleKeyDown(event) {
+        if (event.keyCode == 13 /*enter*/) {
+            alert('Top!');
+        }
+        if (event.keyCode == 27 /*esc*/) {
+            alert('Oh non');
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <form action="#">
                     <textarea id= 'writerTextarea' className="WriterTextarea" defaultValue="Non mais c'est pas vrai?!"/>
+                    <button onClick={this.saveAction} className='saver'>Enregistrer</button>
                 </form>
             </div>
         );
